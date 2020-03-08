e83e503c21cadb4b86074aaef6ba29b5e72362c2
Rafal66 <sprengel.rafal@gmail.com>
2020-03-08 14:48:30 +0000

    Updates

---
+++ precache-manifest.fef692dc62f6f32b3feaf1ade3e3620d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dff22056e41faf7058e5b05e320d952e",
+    "url": "/react-ToDo/index.html"
+  },
+  {
+    "revision": "7ade92beffca8d72d53b",
+    "url": "/react-ToDo/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "11cda762b681a71eb6fb",
+    "url": "/react-ToDo/static/css/main.98323933.chunk.css"
+  },
+  {
+    "revision": "7ade92beffca8d72d53b",
+    "url": "/react-ToDo/static/js/2.93b116fc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-ToDo/static/js/2.93b116fc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "11cda762b681a71eb6fb",
+    "url": "/react-ToDo/static/js/main.87eabc12.chunk.js"
+  },
+  {
+    "revision": "612d08c8ca8335ae0f01",
+    "url": "/react-ToDo/static/js/runtime-main.d6c8c1f3.js"
+  }
+]);
